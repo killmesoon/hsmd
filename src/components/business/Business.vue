514478a4821ec9113bfebeea57772b9bc647a978
DONG <1049488929@qq.com>
2019-12-12 21:17:52 +0800

    init

--- src/components/business/Business.vue
+++ src/components/business/Business.vue
@@ -1,7 +1,35 @@
 <template>
     <div class="main">
         <div class="banner">
-            <img src="" alt="">
+            <img src="../../assets/img/business/business_banner.png" alt="">
+        </div>
+        <div class="business-container">
+            <div class="content">
+                <div class="box">
+                    <div class="pic-box">
+                        <img src="../../assets/img/business/wyn.jpg" alt="">
+                    </div>
+                    <div class="pic-box">
+                        <img src="../../assets/img/business/green.jpg" alt="">
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="pic-box">
+                        <img src="../../assets/img/business/mh.jpg" alt="">
+                    </div>
+                    <div class="pic-box">
+                        <img src="../../assets/img/business/y.jpg" alt="">
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="pic-box">
+                        <img src="../../assets/img/business/yd.jpg" alt="">
+                    </div>
+                    <div class="pic-box">
+                        <img src="../../assets/img/business/yx.jpg" alt="">
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -13,5 +41,28 @@
 </script>
 
 <style scoped>
+    .banner img {
+        width: 100%;
+    }
 
+    .business-container {
+        width: 100%;
+    }
+
+    .content {
+        width: 40%;
+        margin: 0 auto;
+        height: 828px;
+        background: url("../../assets/img/business/business-bg.png") no-repeat;
+    }
+
+    .box {
+        display: flex;
+    }
+
+    .pic-box {
+        flex: 1;
+        text-align: center;
+        margin-top: 50px;
+    }
 </style>
